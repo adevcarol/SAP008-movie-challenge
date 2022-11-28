c05c6ace4ada560e3e3d7b266b4b7add022433ce
adevcarol <alvscarolina@outlook.com>
2022-11-28 16:28:39 -0300

    Criação do projeto em vue.js e configuração inicial

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>Movie Challenge</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui commodi explicabo rerum dicta similique, facilis odit sit neque! Consectetur, sit voluptates. Nobis aliquam aut fuga, iure recusandae quia pariatur at. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam perferendis non a, laboriosam id dolor fuga amet voluptatem quo, dolorem optio nam nesciunt. Tempore nostrum libero iusto earum quia cumque?</p>
+    <button>More</button>
+    <span id="btn"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 2rem;
+}
+</style>
