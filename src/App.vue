00248b4b7d557f352fb0f8f8ea2a80dcf77b0b9d
adevcarol <alvscarolina@outlook.com>
2022-12-14 18:29:27 -0300

    Configuração inicial da função de ordenar filmes

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,26 @@
   <section id="app">
     <h1>Movie Challenge</h1>
     <h4>Top Filmes by themoviedb</h4>
-      <div class="movies">
+      <select v-model="selected" class="order-select">
+        <option>Ascending</option>
+        <option>Descending</option>
+      </select>
+      <section class="movies">
         <div v-for="movie, i in movies" :key="i" class="movie">
-          <p>{{ movie.title }}</p>
-          <img v-bind:src="`https://image.tmdb.org/t/p/w500/${ movie.poster_path }`" />
+          <h3>{{ movie.title }}</h3>
+          <div class="cover" id="cover">
+            <img v-bind:src="`https://image.tmdb.org/t/p/w500${ movie.poster_path }`" v-on:click="doSomething"/>
+            <p>{{ movie.release_date }}</p>
+          </div>
         </div>
-      </div>
+      </section>
   </section>
 </template>
 
 <script>
 import { onMounted, defineComponent, ref } from "vue";
 import api from "./services/api.js";
+// import { test } from "./scripts/index.js"
 
 export default defineComponent({
   name: 'App',
@@ -26,18 +34,21 @@ export default defineComponent({
       });
     onMounted(fetchMovies);
     return { movies }
-  }
+  },
+  
 });
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
 * {
   padding: 0;
   margin: 0;
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Poppins', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -45,24 +56,47 @@ export default defineComponent({
   margin-top: 2rem;
 }
 
+.order-select {
+  margin-top: 1rem;
+  padding: 0.2rem;
+  border: none;
+  border-radius: 0.2rem;
+  background-color: #2c3e50;
+  color: #fff;
+  cursor: pointer;
+}
 .movies {
+  margin-top: 2rem;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: center;
 }
 
+.movies h3 {
+  font-size: 1.2rem;
+}
+
 .movie {
   width: 12rem;
+  height: 18rem;
   margin: 1rem;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  gap: 1rem;
+  gap: 0.2rem;
 }
 
-img {
+.cover {
+  width: 100%;
+}
+
+.cover p {
+  font-size: 0.8rem;
+}
+
+.cover img {
   width: 68%;
   border-radius: 0.6rem;
 }
---
+++ src/scripts/index.js
@@ -0,0 +1,4 @@
+
+export const test = () => {
+  return console.log('função de teste');
+};
